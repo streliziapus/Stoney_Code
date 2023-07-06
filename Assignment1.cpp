428d917d73d51a21b64632a312c75f9e93301d0b
Roman <romanliu1220@gmail.com>
2023-07-06 00:16:55 -0700

    Commiting Assignment1.cpp

---
+++ Assignment1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int myInt = 15;
+    double myDouble = 12.99;
+    char myChar = 'R';
+    string myString = "Stoney";
+    bool myBool = true;
+
+    /**
+     * Assignment!
+     * Description: Print out 2 sentences using the variables above
+     * EX: My cat is 15 pounds and Stoney is 12.99.
+     * EX: it is true that Roman's name starts with R.
+    */
+
+}
